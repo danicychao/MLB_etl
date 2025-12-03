c0f3abcca2882e3744fa16c6e3169c920d96cdee
danichao <danichao@gapp.nthu.edu.tw>
2025-12-03 20:03:25 +0100

    add: match team_name

---
+++ sql/homerun_leaderboard_query.sql
@@ -0,0 +1,16 @@
+WITH home_run_leaders AS (
+	SELECT
+	max(batter_name) AS batter_name,
+	sum(home_run) AS home_runs,
+	max(team_id) AS team_id
+	FROM game_details_batter
+	GROUP BY batter_id
+	ORDER BY home_runs DESC
+	LIMIT 10)
+
+SELECT
+t.team_name as team_name,
+hr.batter_name as batter_name,
+hr.home_runs as home_runs
+FROM home_run_leaders as hr LEFT JOIN teams as t
+ON CAST(hr.team_id AS INTEGER) = t.id
--- sql/homerun_rank_query.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-max(batter_name) AS batter_name,
-sum(home_run) AS home_runs
-FROM game_details_batter
-GROUP BY batter_id
-ORDER BY home_runs DESC
